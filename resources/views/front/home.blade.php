62d8e2fc591c7dd78fdf56e1c372b37f0678f2f4
creamynald <creamynald@gmail.com>
2022-04-27 12:02:13 +0700

    fitur sambutan kepala dinas

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,33 +1,46 @@
 @extends('front.layouts.frontend')
 
 @section('content')
-<!-- ======= About Section ======= -->
-<section id="about" class="about">
+<!-- ======= sambutan kadis Section ======= -->
+<section id="team" class="team">
   <div class="container">
+    <div class="row">
 
-    <div class="row content">
-      <div class="col-lg-6">
-        <h2>Eum ipsam laborum deleniti velitena</h2>
-        <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assum perenda sruen jonee trave</h3>
+      <div class="col-lg-8">
+        <div class="sambutan">
+          <div class="member align-items-start">
+            <h3><b>Sambutan Kepala Dinas</b></h3>
+            <p>Pertama dan utama sekali, mari kita panjatkan puji syukur ke hadirat Allah SWT., Tuhan Yang Maha Kuasa, karena berkat cucuran rahmat dan nikmat-Nya, Dinas Komunikasi, Informatika dan Statistik, sebagai salah satu Perangkat Daerah di Pemerintah Kabupaten (Pemkab) Bengkalis, masih bisa dan terus berupaya untuk eksis dalam mendukung terwujudnya visi Kabupaten Bengkalis, yakni; <br><br> “Terwujudnya Kabupaten Bengkalis yang Bermarwah, Maju dan Sejahtera”, khususnya melalui transparansi dan keterbukaan informasi publik“</p>
+            <div class="d-flex align-items-start mt-3">
+              <div class="minipic"><img src="{{ asset('img/kepaladinas.jpg') }}" class="img-fluid" alt=""></div>
+              <div class="member-info">
+                <h4>Hendrik Dwi Yatmoko, S.Sos, MT</h4>
+                <span>Kepala Dinas</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="col-lg-6 pt-4 pt-lg-0">
-        <p>
-          Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
-        </p>
-        <ul>
-          <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequa</li>
-          <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-          <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</li>
-        </ul>
-        <p class="fst-italic">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua.
-        </p>
+
+      <div class="col-lg-4 mt-4 mt-lg-0">
+        <div class="member d-flex align-items-start">
+          <div class="pic"><img src="/img/team/team-2.jpg" class="img-fluid" alt=""></div>
+          <div class="member-info">
+            <h4>Sarah Jhonson</h4>
+            <span>Product Manager</span>
+            <p>Aut maiores voluptates amet et quis praesentium qui senda para</p>
+            <div class="social">
+              <a href=""><i class="ri-twitter-fill"></i></a>
+              <a href=""><i class="ri-facebook-fill"></i></a>
+              <a href=""><i class="ri-instagram-fill"></i></a>
+              <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+            </div>
+          </div>
+        </div>
       </div>
+
     </div>
 
   </div>
-</section><!-- End About Section -->
+</section><!-- End Team Section -->
 @endsection
